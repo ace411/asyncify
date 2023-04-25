47a07a79ae9ee9dd1759a372ba06a9d3274f51f3
ace411 <lochbm@gmail.com>
2023-04-25 16:59:41 +0300

    fix: modified error format

--- tests/AsyncTest.php
+++ tests/AsyncTest.php
@@ -24,14 +24,14 @@ class AsyncTest extends TestCase
           '(function (...$args) { if (!\is_file($args[0])) { throw new \Exception("Could not find file: " . $args[0]); } return \file_get_contents($file); })',
           [12],
         ],
-        'Exception: Could not find file: 12',
+        'Could not find file: 12',
       ],
       // native PHP function
       [
         ['file_get_contents', ['foo.txt']],
         concat(
           ' ',
-          'Exception: file_get_contents(foo.txt):',
+          'file_get_contents(foo.txt):',
           PHP_VERSION_ID >= 80000 ? 'Failed' : 'failed',
           'to open stream: No such file or directory'
         ),
@@ -41,7 +41,7 @@ class AsyncTest extends TestCase
         ['file_get_contents', []],
         concat(
           ' ',
-          'Exception: file_get_contents() expects at least 1',
+          'file_get_contents() expects at least 1',
           PHP_VERSION_ID >= 80000 ? 'argument,' : 'parameter,',
           '0 given'
         ),
@@ -52,7 +52,7 @@ class AsyncTest extends TestCase
           '(function ($file) { if (!\is_file($file)) { trigger_error("Could not find file " . $file); } return \file_get_contents($file); })',
           ['foo.txt'],
         ],
-        'Exception: Could not find file foo.txt',
+        'Could not find file foo.txt',
       ],
       // check if objects can be passed
       [
