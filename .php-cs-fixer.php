ddd3fdba35bd8b1066146ad6e111c0c6fa91e0b2
ace411 <lochbm@live.com>
2025-02-25 11:43:58 +0300

    style: update CS fixer configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,7 +3,7 @@
 /**
  * php-cs file
  * contains rubric for code-style fixes
- * 
+ *
  * @package chemem/asyncify
  * @author Lochemem Bruno Michael
  * @license Apache-2.0
@@ -15,18 +15,24 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
-  ->exclude(['vendor', 'cache', 'bin'])
+  ->exclude(['vendor', 'cache'])
   ->in(__DIR__);
 
 $config = new Config;
 
 return $config
-  ->setRules([
-    '@PSR12'                      => true,
-    'linebreak_after_opening_tag' => true,
-    'binary_operator_spaces'      => [
-      'operators' => ['=>' => 'align', '=' => 'align'],
-    ],
-  ])
+  ->setRules(
+    [
+      '@PSR12'                      => true,
+      'linebreak_after_opening_tag' => true,
+      'trailing_comma_in_multiline' => false,
+      'binary_operator_spaces'      => [
+        'operators'                 => [
+          '=>'                      => 'align',
+          '='                       => 'align',
+        ],
+      ],
+    ]
+  )
   ->setFinder($finder)
   ->setIndent('  ');
