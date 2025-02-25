737ee300d4c0b04d3bd037ffa02974720bdafc8a
ace411 <lochbm@live.com>
2025-02-25 11:46:19 +0300

    feat: add thread function

--- src/index.php
+++ src/index.php
@@ -11,4 +11,5 @@
 require_once __DIR__ . '/internal/asyncify.php';
 require_once __DIR__ . '/internal/constants.php';
 require_once __DIR__ . '/internal/proc.php';
+require_once __DIR__ . '/internal/thread.php';
 require_once __DIR__ . '/call.php';
