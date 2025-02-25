41a147bcb130601fa093009cfb438b8183091eba
ace411 <lochbm@live.com>
2025-02-25 12:04:10 +0300

    test: revamp testsuite

--- tests/AsyncTest.php
+++ tests/AsyncTest.php
@@ -13,87 +13,53 @@ use function Chemem\Bingo\Functional\toException;
 use function React\Async\await;
 use function React\Promise\resolve;
 
+use const Chemem\Asyncify\Internal\PHP_THREADABLE;
+
 class AsyncTest extends TestCase
 {
-  public function asyncifyProvider(): array
+  public static function asyncifyProvider(): array
   {
     return [
-      // invalid call to user-specified function
       [
         [
-          '(function (...$args) { if (!\is_file($args[0])) { throw new \Exception("Could not find file: " . $args[0]); } return \file_get_contents($file); })',
-          [12],
+          (
+            PHP_THREADABLE ?
+              function (...$args) {
+                return \file_get_contents(...$args);
+              } :
+              <<<'PHP'
+              (
+                function (...$args) {
+                  return \file_get_contents(...$args);
+                }
+              )
+              PHP
+          ),
+          ['foo.txt']
         ],
-        'Could not find file: 12',
+        '/(No such file or directory)/i'
       ],
-      // native PHP function
       [
-        ['file_get_contents', ['foo.txt']],
-        concat(
-          ' ',
-          'file_get_contents(foo.txt):',
-          PHP_VERSION_ID >= 80000 ? 'Failed' : 'failed',
-          'to open stream: No such file or directory'
-        ),
+        ['exec', ['echo "foo"']],
+        '/^(foo)$/'
       ],
-      // erroneous call to native PHP function
-      [
-        ['file_get_contents', []],
-        concat(
-          ' ',
-          'file_get_contents() expects at least 1',
-          PHP_VERSION_ID >= 80000 ? 'argument,' : 'parameter,',
-          '0 given'
-        ),
-      ],
-      // trigger error in user-defined function
       [
         [
-          '(function ($file) { if (!\is_file($file)) { trigger_error("Could not find file " . $file); } return \file_get_contents($file); })',
-          ['foo.txt'],
+          <<<'PHP'
+          (
+            function ($cmd) {
+              return exec($cmd);
+            }
+          )
+          PHP,
+          ['echo "foo"'],
         ],
-        'Could not find file foo.txt',
-      ],
-      // check if objects can be passed
-      [
-        [
-          '(function (object $list) { return $list->foo; })',
-          [(object)['foo' => 'foo']],
-        ],
-        'foo',
-      ],
-      // check if arrays can be passed
-      [
-        [
-          '(function (array $list) { return $list["foo"]; })',
-          [['foo' => 'foo']],
-        ],
-        'foo',
-      ],
-      // check if numbers can be passed
-      [
-        [
-          '(function (int $x) { return $x + 10; })',
-          [10],
-        ],
-        20,
-      ],
-      // check if objects can be returned
-      [
-        [
-          '(function (string $x) { return (object)["foo" => $x]; })',
-          ['foo'],
-        ],
-        (object)['foo' => 'foo'],
-      ],
-      // check if arrays can be returned
-      [
-        [
-          '(function (string $x) { return ["foo" => $x]; })',
-          ['foo'],
-        ],
-        ['foo' => 'foo'],
-      ],
+        (
+          PHP_THREADABLE ?
+            '/(Call to undefined function)/i' :
+            '/^(foo)$/'
+        )
+      ]
     ];
   }
 
@@ -111,7 +77,10 @@ class AsyncTest extends TestCase
       }
     )(...$args);
 
-    $this->assertEquals($result, $exec);
+    $this->assertMatchesRegularExpression(
+      $result,
+      $exec
+    );
   }
 
   /**
@@ -129,6 +98,9 @@ class AsyncTest extends TestCase
       }
     )(...$args);
 
-    $this->assertEquals($result, $exec);
+    $this->assertMatchesRegularExpression(
+      $result,
+      $exec
+    );
   }
 }
