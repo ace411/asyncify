ab5ee651a13576a0311bf7b04ea473cc55acdc53
ace411 <lochbm@live.com>
2025-02-25 12:07:12 +0300

    feat: define PHP_THREADABLE

--- src/internal/constants.php
+++ src/internal/constants.php
@@ -12,20 +12,37 @@ declare(strict_types=1);
 
 namespace Chemem\Asyncify\Internal;
 
+use ReactParallel\EventLoop\EventLoopBridge;
+use ReactParallel\Runtime\Runtime;
+
+/**
+ * @var bool PHP_THREADABLE flag with which to ascertain presence of CSP utilities
+ */
+\define(
+  __NAMESPACE__ . '\\PHP_THREADABLE',
+  (
+    \extension_loaded('parallel') &&
+    \class_exists(EventLoopBridge::class) &&
+    \class_exists(Runtime::class)
+  )
+);
+
 /**
  * @var PHP_EXECUTABLE_TEMPLATE boilerplate for asynchronous execution of a PHP function
  */
 const PHP_EXECUTABLE_TEMPLATE = <<<'PHP'
-function handleException(Throwable $exception): void
-{
-  echo $exception->getMessage();
-}
-function handleError(...$args)
-{
-  throw new \Exception($args[1]);
-}
-\set_error_handler("handleError", E_ALL);
-\set_exception_handler("handleException");
+\set_error_handler(
+  function (...$args) {
+    [$errno, $errmsg] = $args;
+    throw new \Exception($errmsg, $errno);
+  },
+  E_ALL
+);
+\set_exception_handler(
+  function (Throwable $err) {
+    echo $err->getMessage();
+  }
+);
 require_once "%s";
 echo \base64_encode(
   \serialize(
